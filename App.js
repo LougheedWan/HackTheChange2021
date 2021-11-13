c866749a84d6289f321989aee4023789b2ec2a24
LougheedWan <47652087+LougheedWan@users.noreply.github.com>
2021-11-13 13:47:40 -0700

    pages

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import parseErrorStack from 'react-native/Libraries/Core/Devtools/parseErrorStack';
+import Login from './screens/login'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      <Login></Login>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { ScrollView, StyleSheet, View, Text, Button} from "react-native";
+
+const LoginScreen = ({ nav }) => {
+    return (
+        <View style = {styles.form}>
+        <ScrollView >
+            <Text style = {styles.title}>Welcome!
+            </Text>
+            <input></input>
+            <input></input>
+            <Button>Login</Button>
+        </ScrollView>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    form: {
+        
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 30,
+        color: 'blue'
+    }
+});
+
+export default LoginScreen;
