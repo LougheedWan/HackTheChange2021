723906bed532ce34efa91a1b26c40468a147285a
LougheedWan <47652087+LougheedWan@users.noreply.github.com>
2021-11-13 18:54:59 -0700

    login_frameworks

--- screens/login.js
+++ screens/login.js
@@ -1,30 +1,67 @@
-import React from "react";
-import { ScrollView, StyleSheet, View, Text, Button, Alert} from "react-native";
+import React, {useState} from "react";
+import { ScrollView, StyleSheet, View, Text, Alert} from "react-native";
+import {Card, Title, Button, TextInput} from 'react-native-paper';
+
 
 const LoginScreen = ({ navigation }) => {
 
+    const authorize = () => {
+
+        // as of right now we do hard coded authorization:
+        // very insecure way of logging in
+        if (un == "admin" && pw == "123"){
+            alert("success");
+            navigation.navigate('Dashboard');
+        }
+        else {
+            alert("wrong creds");
+        }
+    }
+
+    const newUser = () => {
+        navigation.navigate('Signup')
+    }
+    const [un, setUn] = useState('');
+    const [pw, setPw] = useState('');
+
     return (
-        <View style = {styles.form}>
-        <ScrollView >
-            <Text style = {styles.title}>Welcome
-            </Text>
-            <input placeholder = "Username"></input>
-            <input placeholder = "Password"></input>
-            <Button title="Login" onPress={() => 
-                navigation.navigate('Dashboard')} />
-        </ScrollView>
-        </View>
+        <Card mode = "elevated" style = {styles.form}>
+            <Card.Content style = {styles.form}>
+            <Title style = {styles.title}>Welcome</Title>
+            <TextInput placeholder = "Username" onChangeText = {un => setUn(un)}></TextInput>
+            <TextInput type = "password"  placeholder = "Password" onChangeText = {pw => setPw(pw)}></TextInput>
+            </Card.Content>
+            <Card.Actions style = {styles.form}>
+            <Button onPress={authorize}>Login</Button>
+            <Button onPress={newUser}>SignUp</Button>
+            </Card.Actions>
+        </Card>
     )
 };
 
 const styles = StyleSheet.create({
     form: {
-        alignItems: 'center'
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10
+        
     },
     title: {
         fontWeight: 'bold',
         fontSize: 30,
-        color: 'blue'
+        color: 'blue',
+        paddingBottom: 30,
+        paddingTop: 30
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+    loginButton: {
+        marginTop: 30
     }
 });
 
